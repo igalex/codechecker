949fecc1246720043f28a38356cdca5ecf3c5907
Alex Gábor Ispánovics <gabor.alex.ispanovics@ericsson.com>
2015-08-05 15:48:09 +0200

    Add ELF executable detection

--- external-source-deps/build-logger/src/ldlogger-hooks.c
+++ external-source-deps/build-logger/src/ldlogger-hooks.c
@@ -10,14 +10,19 @@
 #define _GNU_SOURCE
 #endif
 
+#include <linux/limits.h>
 #include <dlfcn.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <paths.h>
+#include <elf.h>
 
 #include "ldlogger-hooks.h"
 
@@ -53,6 +58,154 @@ static void unsetLDPRELOAD(const char* const filename_, ...)
 }
 
 /**
+ * Reads the given file descriptor and if it seems to be a valid ELF binary, it
+ * returns 1.
+ *
+ * @param fd a file descriptor to read
+ * @return 1 if the file is an ELF binary, 0 if not, -1 on error.
+ */
+static int isElf(int fd)
+{
+  unsigned char e_ident[EI_NIDENT];
+  ssize_t rs;
+
+  rs = read(fd, e_ident, sizeof(e_ident));
+  if (rs != sizeof(e_ident) || rs == -1)
+  {
+    return -1;
+  }
+
+  if (e_ident[EI_MAG0] == ELFMAG0 &&
+      e_ident[EI_MAG1] == ELFMAG1 &&
+      e_ident[EI_MAG2] == ELFMAG2 &&
+      e_ident[EI_MAG3] == ELFMAG3)
+  {
+    return 1;
+  }
+
+  return 0;
+}
+
+/**
+ * Resolves the given command and opens its executable.
+ *
+ * @param filename a command or a file path
+ * @return an opened file descriptor or -1 on error.
+ */
+static int openCommandExecutable(const char* filename_)
+{
+  if (strchr(filename_, '/'))
+  {
+    /* easy case: it's a relative or absolute path */
+    return open(filename_, O_RDONLY);
+  }
+  else
+  {
+    /* Let's resolve the command using the PATH env. var. */
+    char buff[PATH_MAX];
+    size_t cmdlen;
+
+    const char* PATH = getenv("PATH");
+    if (!PATH)
+    {
+      PATH = _PATH_DEFPATH;
+    }
+
+    cmdlen = strlen(filename_);
+    do
+    {
+      size_t plen;
+      int fd;
+      const char* elemStart = PATH;
+
+      /* Find the end of the PATH element */
+      PATH = strchr(elemStart, ':');
+      if (!PATH)
+      {
+        PATH = strchr(elemStart, '\0');
+      }
+
+      if (elemStart == PATH)
+      {
+        /* Double colons in $PATH means to search the current directory */
+        buff[0] = '.';
+        plen = 1;
+      }
+      else
+      {
+        /* Simple PATH elem */
+        plen = PATH - elemStart;
+        memcpy(buff, elemStart, plen);
+      }
+
+      if (plen + cmdlen + 2 > sizeof(buff))
+      {
+        /* The path is too long */
+        continue;
+      }
+
+      buff[plen] = '/';
+      memcpy(buff + plen + 1, filename_, cmdlen);
+      buff[plen + cmdlen + 1] = '\0';
+
+      if (access(buff, X_OK) != 0)
+      {
+        /* not an executable */
+        continue;
+      }
+
+      fd = open(buff, O_RDONLY);
+      if (fd != -1)
+      {
+        return fd;
+      }
+    } while (*PATH != '\0' && *(++PATH) != '\0');
+  }
+
+  return -1;
+}
+
+/**
+ * Decides whether the given program should be logged or not.
+ *
+ * @param filename_ a command or a file path
+ * @return non-zero if should log this command, 0 otherwise
+ */
+static int shouldLog(const char* filename_)
+{
+  int res = 0;
+  int fd;
+  const char* binOnly = getenv("CC_LOG_BIN_ONLY");
+
+  if (!binOnly || binOnly[0] != '1')
+  {
+    return 1;
+  }
+
+  if (!filename_)
+  {
+    return 0;
+  }
+
+  fd = openCommandExecutable(filename_);
+  if (fd == -1)
+  {
+    /* can't open: if we can't decide by file content, let it go */
+    return 1;
+  }
+
+  if (isElf(fd) != 0)
+  {
+    /* It's an elf executable or we can't decide (error) */
+    res = 1;
+  }
+
+  close(fd);
+
+  return res;
+}
+
+/**
  * Tries to log an exec* call.
  *
  * @param origin_ the exec* function name.
@@ -65,7 +218,11 @@ static void tryLog(
 {
   size_t i;
   const char* loggerArgs[CC_LOGGER_MAX_ARGS];
-  char* ldpreload;
+
+  if (!shouldLog(filename_))
+  {
+    return;
+  }
 
   loggerArgs[0] = filename_;
   for (i = 0; argv_[i]; ++i)
@@ -74,7 +231,7 @@ static void tryLog(
   }
   loggerArgs[i+1] = NULL;
 
-  logExec(i+1, (char* const*) loggerArgs);
+  logExec(i+1, loggerArgs);
 }
 
 __attribute__ ((visibility ("default"))) int execv(const char* filename_, char* const argv_[])
